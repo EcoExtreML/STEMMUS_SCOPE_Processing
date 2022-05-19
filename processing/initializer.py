ac0191d29360e401df4e5c264dd0a558466de65d
Yang <y.liu@esciencecenter.nl>
2022-05-19 14:35:47 +0200

    create python module frame

---
+++ processing/initializer.py
@@ -0,0 +1,18 @@
+from asyncio.log import logger
+from pathlib import Path
+import logging
+
+
+class InputDir:
+    """Create input directories and copy required data."""
+
+    def __init__(self, config):
+        """Load """
+
+    def _copy_data():
+        """Copy required data to the work directory."""
+        # check all files/folders
+        #logger
+        # required data i.e. directional, fluspect_parameters, leafangles, radiationdata, soil_spectra, and input_data.xlsx
+
+    
