e4ba3b76496e612ecc65fab3b443ed97811a11f6
Yang <y.liu@esciencecenter.nl>
2022-06-01 23:07:28 +0200

    add tests for prepare work dir function

--- tests/test_initializer.py
+++ tests/test_initializer.py
@@ -10,8 +10,26 @@ class TestInputDir:
         # path to executable
         path_to_model = Path("path_to_STEMMUS_SCOPE_repository")
         # path to config file
-        path_to_config_file = data_folder / "config_file_snellius.txt"
+        path_to_config_file = data_folder / "config_file_test.txt"
         # Instantiate working directories handler from PyStemmusScope
         initializer = InputDir(path_to_config_file, path_to_model)
 
         assert isinstance(initializer, InputDir)
+
+    def test_prepare_work_dir(self):
+        # path to executable
+        path_to_model = Path("path_to_STEMMUS_SCOPE_repository")
+        # path to config file
+        path_to_config_file = data_folder / "config_file_test.txt"
+        # Instantiate working directories handler from PyStemmusScope
+        initializer = InputDir(path_to_config_file, path_to_model)
+        # specify the forcing filenames
+        forcing_filenames_list = ["NL-fake_1979-2021_FLUXNET2010_Met.nc"]
+        # prepare work directory
+        work_dir_dict, config_path_dict = initializer.prepare_work_dir(forcing_filenames_list)
+
+        assert work_dir_dict["NL-fake_1979-2021_FLUXNET2010_Met.nc"].is_dir()
+        assert config_path_dict["NL-fake_1979-2021_FLUXNET2010_Met.nc"].exists()
+
+
+          
