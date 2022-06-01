25a1cb284a813d0129e7d6506872da31ffaf4d8a
Yang <y.liu@esciencecenter.nl>
2022-06-01 11:12:33 +0200

    function to read config file

--- PyStemmusScope/initializer.py
+++ PyStemmusScope/initializer.py
@@ -6,8 +6,26 @@ import logging
 class InputDir:
     """Create input directories and copy required data."""
 
-    def __init__(self, config):
-        """Load """
+    def __init__(self, path_to_config_file: str = "config_file_snellius.txt",
+        path_to_model: str = "path_to_STEMMUS_SCOPE_repository"
+    ):
+        """Instantiate a handler for working directories
+
+        Specify path STEMMUS SCOPE repository and load the config file.
+
+        """
+    
+        self.config = self._read_config(path_to_config_file)
+
+    def _read_config(self, path_to_config_file):
+        """Read config from given config file."""
+        config = {}
+        with open(path_to_config_file, "r") as f:
+            for line in f:
+                (key, val) = line.split("=")
+                config[key] = val.rstrip('\n')
+
+        return config
 
     def _copy_data():
         """Copy required data to the work directory."""
