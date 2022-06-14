e2629a99053d652f094343e7bde16bf87bddedc4
Yang <y.liu@esciencecenter.nl>
2022-06-14 16:36:28 +0200

    add doc strings to all methods

--- PyStemmusScope/iostreamer.py
+++ PyStemmusScope/iostreamer.py
@@ -15,17 +15,32 @@ class InputDir:
     def __init__(self, path_to_config_file: str = "config_file_snellius.txt",
         path_to_model: str = "path_to_STEMMUS_SCOPE_repository"
     ):
-        """Instantiate a handler for working directories
+        """Instantiate a handler for work directories.
 
         Specify path STEMMUS SCOPE repository and load the config file.
 
+        Args:
+            path_to_config_file: Path to the config file.
+            path_to_model: Path to the model.
         """
     
         self.config = self._read_config(path_to_config_file)
         self.path_to_model = path_to_model
 
+    def __str__(self):
+        return "Input directories handler."
+
     def _read_config(self, path_to_config_file):
-        """Read config from given config file."""
+        """Read config from given config file.
+
+        Load paths from config file and save them into dict.
+
+        Args:
+            path_to_config_file: Path to the config file.
+
+        Returns:
+            Dictionary containing paths to work directory and all sub-directories.
+        """
         config = {}
         with open(path_to_config_file, "r") as f:
             for line in f:
@@ -37,20 +52,38 @@ class InputDir:
     def prepare_work_dir(self, forcing_filenames_list: list = [],
         full_run: bool = False
     ):
-        """Prepare work directory"""
+        """Prepare work directory for each station.
+
+        Prepare work directory based on the given list of forcing. This function
+        creates directory for each forcing data and name it using the name of the
+        station.
+
+        Args:
+            forcing_filenames_list: List of forcing filenames to be used for the experiments.
+            full_run: If true, all stations with forcing listed in the ForcingPath will be used.
+
+        Returns:
+            Dictionary containing path to work directory and config file for every station/forcing.
+        """
         self.forcing_filenames_list = forcing_filenames_list
         
         if full_run:
             self.forcing_filenames_list = [file.name for file in Path(self.config["ForcingPath"]).iterdir()]
 
-        # dict to store path to config file and working directory for each station
+        # dict to store path to config file and work directory for each station
         self.work_dir_dict, self.config_path_dict = self._create_work_dir()
 
         return self.work_dir_dict, self.config_path_dict
 
     def _create_work_dir(self):
-        """Create input directory and copy forcing files."""
-        # empty dict to store path to config file and working directory for each station
+        """Create input directory and copy forcing files.
+
+        Work flow executor to create work directory and all sub-directories.
+
+        Returns:
+            Dictionary containing path to work directory and config file for every station/forcing.
+        """
+        # empty dict to store path to config file and work directory for each station
         config_path_dict = {}
         work_dir_dict = {}
         for ncfile in self.forcing_filenames_list:
@@ -72,7 +105,13 @@ class InputDir:
         return work_dir_dict, config_path_dict
 
     def _copy_data(self, work_dir):
-        """Copy required data to the work directory."""
+        """Copy required data to the work directory.
+
+        Create sub-directories inside the work directory and copy data.
+
+        Args:
+            work_dir: Path to the work directory.
+        """
         folder_list_vegetation = ["Directional", "FluspectParameters", "Leafangles",
             "Radiationdata", "SoilSpectra"]
         for folder in folder_list_vegetation:
@@ -83,7 +122,19 @@ class InputDir:
         shutil.copy(self.config["InputData"], work_dir)
 
     def _update_config_file(self, ncfile, work_dir, station_name, timestamp):
-        """Update config file for each station."""
+        """Update config file for each station.
+
+        Create config file for each forcing/station under the work directory.
+        
+        Args:
+            ncfile: Name of forcing file.
+            work_dir: Path to the work directory.
+            station_name: Station name inferred from forcing file.
+            timestamp: Timestamp when creating the config file.
+        
+        Returns:
+            Path to updated config file.
+        """
         config_file_path = Path(work_dir, f"{station_name}_{timestamp}_config.txt")
         with open(config_file_path, 'w') as f:
             for i in self.config.keys():
--- tests/test_iostreamer.py
+++ tests/test_iostreamer.py
@@ -12,9 +12,18 @@ class TestInputDir:
         # path to config file
         path_to_config_file = data_folder / "config_file_test.txt"
         # Instantiate working directories handler from PyStemmusScope
-        initializer = InputDir(path_to_config_file, path_to_model)
+        iostreamer = InputDir(path_to_config_file, path_to_model)
 
-        assert isinstance(initializer, InputDir)
+        assert isinstance(iostreamer, InputDir)
+
+    def test_str(self):
+        # path to executable
+        path_to_model = Path("path_to_STEMMUS_SCOPE_repository")
+        # path to config file
+        path_to_config_file = data_folder / "config_file_test.txt"
+        # Instantiate working directories handler from PyStemmusScope
+        iostreamer = InputDir(path_to_config_file, path_to_model)
+        assert str(iostreamer) == "Input directories handler."
 
     def test_prepare_work_dir(self):
         # path to executable
@@ -22,11 +31,11 @@ class TestInputDir:
         # path to config file
         path_to_config_file = data_folder / "config_file_test.txt"
         # Instantiate working directories handler from PyStemmusScope
-        initializer = InputDir(path_to_config_file, path_to_model)
+        iostreamer = InputDir(path_to_config_file, path_to_model)
         # specify the forcing filenames
         forcing_filenames_list = ["NL-dummy_1979-2021_FLUXNET2010_Met.nc"]
         # prepare work directory
-        work_dir_dict, config_path_dict = initializer.prepare_work_dir(forcing_filenames_list)
+        work_dir_dict, config_path_dict = iostreamer.prepare_work_dir(forcing_filenames_list)
 
         assert work_dir_dict["NL-dummy_1979-2021_FLUXNET2010_Met.nc"].is_dir()
         assert config_path_dict["NL-dummy_1979-2021_FLUXNET2010_Met.nc"].exists()
