f5b33e39dad537bfe5a4529b28546a48ae56ac1a
Yang <y.liu@esciencecenter.nl>
2022-06-17 17:51:06 +0200

    re-write class to functions

--- PyStemmusScope/iostreamer.py
+++ PyStemmusScope/iostreamer.py
@@ -7,143 +7,105 @@ import distutils.dir_util
 import distutils.file_util
 import os
 import time
+import logging
 from pathlib import Path
 
+logger = logging.getLogger(__name__)
 
-class InputDir:
-    """Create input directories and copy required data."""
+def read_config(path_to_config_file):
+    """Read config from given config file.
 
-    def __init__(self, path_to_config_file: str = "config_file_snellius.txt",
-        path_to_model: str = "path_to_STEMMUS_SCOPE_repository"
-    ):
-        """Instantiate a handler for work directories.
+    Load paths from config file and save them into dict.
 
-        Specify path STEMMUS SCOPE repository and load the config file.
+    Args:
+        path_to_config_file: Path to the config file.
 
-        Args:
-            path_to_config_file: Path to the config file.
-            path_to_model: Path to the model.
-        """
+    Returns:
+        Dictionary containing paths to work directory and all sub-directories.
+    """
+    config = {}
+    with open(path_to_config_file, "r", encoding="utf8") as f:
+        for line in f:
+            (key, val) = line.split("=")
+            config[key] = val.rstrip('\n')
+
+    return config
+
+def create_io_dir(forcing_filename, config):
+    """Create input directory and copy required files.
+
+    Work flow executor to create work directory and all sub-directories.
+
+    Returns:
+        Dictionary containing path to work directory and config file for every station/forcing.
+    """
+    # get start time with the format Y-M-D-HM
+    timestamp = time.strftime('%Y%m%d_%H%M')
+    station_name = forcing_filename.split('_')[0]
+    # create input directory
+    input_dir = Path(f"{config['WorkDir']}/input/{station_name}_{timestamp}")
+    Path(input_dir).mkdir(parents=True, exist_ok=True)
+    message = f"Prepare work directory {input_dir} for the station: {station_name}"
+    logger.info("%s", message)
+    # copy model parameters to work directory
+    _copy_data(input_dir, config)
+    input_dir = str(input_dir)
+
+    # create output directory
+    output_dir = Path(f"{config['WorkDir']}/output/{station_name}_{timestamp}")
+    output_dir.mkdir(parents=True, exist_ok=True)
+    message = f"Prepare work directory {output_dir} for the station: {station_name}"
+    logger.info("%s", message)
+    output_dir = str(output_dir)
+
+    # update config file for ForcingFileName and InputPath
+    config_file_path = _update_config_file(forcing_filename, input_dir, output_dir,
+        config, station_name, timestamp)
+
+    return input_dir, output_dir, config_file_path
+
+def _copy_data(input_dir, config):
+    """Copy required data to the work directory.
+
+    Create sub-directories inside the work directory and copy data.
+
+    Args:
+        input_dir: Path to the work directory.
+    """
+    folder_list_vegetation = ["Directional", "FluspectParameters", "Leafangles",
+        "Radiationdata", "SoilSpectra"]
+    for folder in folder_list_vegetation:
+        os.makedirs(input_dir / folder, exist_ok=True)
+        distutils.dir_util.copy_tree(str(config[folder]), str(input_dir / folder))
     
-        self.config = self._read_config(path_to_config_file)
-        self.path_to_model = path_to_model
+    # copy input_data.xlsx
+    distutils.file_util.copy_file(str(config["InputData"]), str(input_dir))
 
-    def __str__(self):
-        return "Input directories handler."
+def _update_config_file(nc_file, input_dir, output_dir, config, station_name, timestamp):
+    """Update config file for each station.
 
-    def _read_config(self, path_to_config_file):
-        """Read config from given config file.
+    Create config file for each forcing/station under the work directory.
+    
+    Args:
+        ncfile: Name of forcing file.
+        input_dir: Path to the input directory.
+        output_dir: Path to the output directory.
+        station_name: Station name inferred from forcing file.
+        timestamp: Timestamp when creating the config file.
+    
+    Returns:
+        Path to updated config file.
+    """
+    config_file_path = Path(input_dir, f"{station_name}_{timestamp}_config.txt")
+    with open(config_file_path, 'w', encoding="utf8") as f:
+        for i in config.items():
+            if i[0] == "ForcingFileName":
+                f.write(i[0] + "=" + nc_file + "\n")
+            elif i == "InputPath":
+                f.write(i[0] + "=" + input_dir + "/" + "\n")
+            elif i == "OutputPath":
+                f.write(i[0] + "=" + output_dir + "/" + "\n")
+            else:
+                f.write(i[0] + "=" + i[1] + "\n")        
 
-        Load paths from config file and save them into dict.
-
-        Args:
-            path_to_config_file: Path to the config file.
-
-        Returns:
-            Dictionary containing paths to work directory and all sub-directories.
-        """
-        config = {}
-        with open(path_to_config_file, "r", encoding="utf8") as f:
-            for line in f:
-                (key, val) = line.split("=")
-                config[key] = val.rstrip('\n')
-
-        return config
-
-    def prepare_work_dir(self, forcing_filenames_list: list,
-        full_run: bool = False
-    ):
-        """Prepare work directory for each station.
-
-        Prepare work directory based on the given list of forcing. This function
-        creates directory for each forcing data and name it using the name of the
-        station.
-
-        Args:
-            forcing_filenames_list: List of forcing filenames to be used for the experiments.
-            full_run: If true, all stations with forcing listed in the ForcingPath will be used.
-
-        Returns:
-            Dictionary containing path to work directory and config file for every station/forcing.
-        """
-        self.forcing_filenames_list = forcing_filenames_list
-        
-        if full_run:
-            self.forcing_filenames_list = [file.name for file in Path(self.config["ForcingPath"]).iterdir()]
-
-        # dict to store path to config file and work directory for each station
-        self.work_dir_dict, self.config_path_dict = self._create_work_dir()
-
-        return self.work_dir_dict, self.config_path_dict
-
-    def _create_work_dir(self):
-        """Create input directory and copy forcing files.
-
-        Work flow executor to create work directory and all sub-directories.
-
-        Returns:
-            Dictionary containing path to work directory and config file for every station/forcing.
-        """
-        # empty dict to store path to config file and work directory for each station
-        config_path_dict = {}
-        work_dir_dict = {}
-        for ncfile in self.forcing_filenames_list:
-            # get start time with the format Y-M-D-HM
-            timestamp = time.strftime('%Y%m%d_%H%M')
-            station_name = ncfile.split('_')[0]
-            # create input directory
-            work_dir = Path(self.config["InputPath"], station_name + '_' + timestamp)
-            Path(work_dir).mkdir(parents=True, exist_ok=True)
-            print(f"Prepare work directory {work_dir} for the station: {station_name}")
-            # copy model parameters to work directory
-            self._copy_data(work_dir)
-            # update config file for ForcingFileName and InputPath
-            config_file_path = self._update_config_file(ncfile, work_dir, station_name, timestamp)
-            # save config path and work directory to the dictionaries
-            work_dir_dict[ncfile] = work_dir
-            config_path_dict[ncfile] = config_file_path
-
-        return work_dir_dict, config_path_dict
-
-    def _copy_data(self, work_dir):
-        """Copy required data to the work directory.
-
-        Create sub-directories inside the work directory and copy data.
-
-        Args:
-            work_dir: Path to the work directory.
-        """
-        folder_list_vegetation = ["Directional", "FluspectParameters", "Leafangles",
-            "Radiationdata", "SoilSpectra"]
-        for folder in folder_list_vegetation:
-            os.makedirs(work_dir / folder, exist_ok=True)
-            distutils.dir_util.copy_tree(str(self.config[folder]), str(work_dir / folder))
-        
-        # copy input_data.xlsx
-        distutils.file_util.copy_file(str(self.config["InputData"]), str(work_dir))
-
-    def _update_config_file(self, ncfile, work_dir, station_name, timestamp):
-        """Update config file for each station.
-
-        Create config file for each forcing/station under the work directory.
-        
-        Args:
-            ncfile: Name of forcing file.
-            work_dir: Path to the work directory.
-            station_name: Station name inferred from forcing file.
-            timestamp: Timestamp when creating the config file.
-        
-        Returns:
-            Path to updated config file.
-        """
-        config_file_path = Path(work_dir, f"{station_name}_{timestamp}_config.txt")
-        with open(config_file_path, 'w', encoding="utf8") as f:
-            for i in self.config.items():
-                if i[0] == "ForcingFileName":
-                    f.write(i[0] + "=" + ncfile + "\n")
-                elif i == "InputPath":
-                    f.write(i[0] + "=" + str(work_dir) + "/" + "\n")
-                else:
-                    f.write(i[0] + "=" + i[1] + "\n")        
-
-        return config_file_path
+    return str(config_file_path)
