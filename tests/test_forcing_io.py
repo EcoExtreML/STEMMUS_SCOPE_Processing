292c7feea132621f2eebfab6ab9ae3a4aee3416a
Bart Schilperoort <b.schilperoort@gmail.com>
2022-06-21 17:18:27 +0200

    Additional tests and a full routine for forcing io

--- tests/test_forcing_io.py
+++ tests/test_forcing_io.py
@@ -4,50 +4,117 @@ import numpy as np
 import pandas as pd
 from PyStemmusScope import forcing_io
 
+numbers = '0123456789'
 
-@pytest.fixture(autouse=True)
+
+def eval_element(el):
+    assert el[0] in [' ', '-']
+    assert el[1] in numbers
+    assert el[2] == '.'
+    assert [n in numbers for n in el[3:10]]
+    assert el[10] == 'e'
+    assert el[11] in ['+', '-']
+    assert [n in numbers for n in el[12:14]]
+    assert len(el) == 14
+    return True
+
+
+def eval_str(fdata):
+    for line in fdata.split('\n'):
+        _ = [eval_element(e) for e in line.split('  ')[1:]]
+    return True
+
+
+@pytest.fixture(scope='session', autouse=True)
 def forcing_data():
     forcing_file = './tests/test_data/FI-Hyy_1996-2014_FLUXNET2015_Met.nc'
     return forcing_io.read_forcing_data(forcing_file)
 
 
-def eval_dat():
-    return 0
+@pytest.fixture(scope='session', autouse=True)
+def mdata_file(tmpdir_factory, forcing_data):
+    fn_out = tmpdir_factory.mktemp('data').join('Mdata.txt')
+    forcing_io.write_meteo_file(forcing_data, fn_out)
+    return str(fn_out)
 
 
-def test_mdata(tmp_path, forcing_data):
-    fn_out = os.path.join(tmp_path, 'Mdata.txt')
+@pytest.fixture(scope='session', autouse=True)
+def lai_file(tmpdir_factory, forcing_data):
+    fn_out = tmpdir_factory.mktemp('data').join('LAI_.dat')
+    forcing_io.write_lai_file(forcing_data, fn_out)
+    return str(fn_out)
+
+@pytest.fixture(scope='session', autouse=True)
+def dat_files(tmpdir_factory, forcing_data):
+    fnames = ['t_.dat', 'Ta_.dat', 'Rin_.dat',
+        'Rli_.dat', 'p_.dat', 'u_.dat',
+        'CO2_.dat', 'ea_.dat', 'year_.dat']
+    write_dir = tmpdir_factory.mktemp('data')
+    forcing_io.write_dat_files(forcing_data, write_dir)
+    return fnames, write_dir
+
+def test_mdata(mdata_file):
     fn_expected = './tests/test_data/Mdata.txt'
 
-    forcing_io.write_meteo_file(forcing_data, fn_out)
-
     df_expected = pd.read_fwf(fn_expected)
-    df_written = pd.read_fwf(fn_out)
+    df_written = pd.read_fwf(mdata_file)
 
     np.testing.assert_allclose(df_expected.values, df_written.values, rtol=1e-5)
 
 
-def test_lai_file(tmp_path, forcing_data):
-    fn_out = os.path.join(tmp_path, 'LAI_.dat')
+# Verifying that the actual data file passes the evaluation
+def test_true_mdata_format():
+    with open('./tests/test_data/Mdata.txt', encoding='utf-8') as f:
+        content_exp = f.read()
+    assert eval_str(content_exp)
+
+
+# Apply the data format evaluation to the python-generated file
+def test_mdata_format(mdata_file):
+    with open(mdata_file, encoding='utf-8') as f:
+        content = f.read()
+    assert eval_str(content)
+
+
+def test_lai_file(lai_file):
     fn_expected = './tests/test_data/LAI_.dat'
 
-    forcing_io.write_lai_file(forcing_data, fn_out)
-
     df_expected = pd.read_fwf(fn_expected)
-    df_written = pd.read_fwf(fn_out)
+    df_written = pd.read_fwf(lai_file)
 
     np.testing.assert_allclose(df_expected.values, df_written.values, rtol=1e-5)
 
 
-def test_dat_files(tmp_path, forcing_data):
+def test_lai_file_format(lai_file):
+    with open(lai_file, encoding='utf-8') as f:
+        content = f.read()
+    assert eval_str(content)
+
+
+def test_dat_files(dat_files):
     expected_path = './tests/test_data/'
-    files = ['t_.dat', 'Ta_.dat', 'Rin_.dat',
-            'Rli_.dat', 'p_.dat', 'u_.dat',
-            'CO2_.dat', 'ea_.dat', 'year_.dat']
 
-    forcing_io.write_dat_files(forcing_data, tmp_path)
+    fnames, write_dir = dat_files
 
-    for fname in files:
+    for fname in fnames:
         df_expected = pd.read_fwf(os.path.join(expected_path, fname))
-        df_written = pd.read_fwf(os.path.join(tmp_path, fname))
+        df_written = pd.read_fwf(os.path.join(write_dir, fname))
         np.testing.assert_allclose(df_expected.values, df_written.values, rtol=1e-5)
+
+
+def test_dat_file_format(dat_files):
+    fnames, write_dir = dat_files
+    for fname in fnames:
+        with open(os.path.join(write_dir, fname), encoding='utf-8') as f:
+            eval_str(f.read())
+
+
+def test_full_routine(tmp_path, dat_files):
+    forcing_file = './tests/test_data/FI-Hyy_1996-2014_FLUXNET2015_Met.nc'
+    forcing_io.prepare_forcing(tmp_path, forcing_file)
+    fnames, _ = dat_files
+    expected_files = fnames + ['LAI_.dat', 'Mdata.txt']
+    for file in expected_files:
+        assert os.path.exists(
+            os.path.join(tmp_path, file)
+        )
