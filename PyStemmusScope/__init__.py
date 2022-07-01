a0824c2d7fd4774bfe0a205240df3ed8d4703447
Bart Schilperoort <b.schilperoort@gmail.com>
2022-07-01 11:01:01 +0200

    Made Sarah's suggested modifications to the notebooks

--- PyStemmusScope/__init__.py
+++ PyStemmusScope/__init__.py
@@ -2,7 +2,8 @@
 import logging
 from .iostreamer import create_io_dir
 from .iostreamer import read_config
-
+from . import iostreamer
+from . import forcing_io
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
 
