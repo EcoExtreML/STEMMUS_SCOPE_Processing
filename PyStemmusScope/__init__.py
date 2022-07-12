e199efb84653631e5a2b624cd89f22ff10d1c876
SarahAlidoost <f.alidoost@esciencecenter.nl>
2022-07-12 18:22:44 +0200

    remove unused imports from init file

--- PyStemmusScope/__init__.py
+++ PyStemmusScope/__init__.py
@@ -1,10 +1,5 @@
 """Documentation about PyStemmusScope"""
 import logging
-from . import forcing_io
-from . import iostreamer
-from .iostreamer import create_io_dir
-from .iostreamer import read_config
-
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
 
