409997a987dc28d7ac3f464478b9cee5b9d029c2
SarahAlidoost <f.alidoost@esciencecenter.nl>
2022-07-13 17:19:09 +0200

    add function to_absolute_path, refactor config_io to use it

--- PyStemmusScope/utils.py
+++ PyStemmusScope/utils.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 import numpy as np
 
 
@@ -22,3 +23,35 @@ def convert_to_lsm_coordinates(lat, lon):
         lon += 720
 
     return np.round(lat).astype(int), np.round(lon).astype(int)
+
+
+def to_absolute_path(
+    input_path: str,
+    parent: Path = None,
+    must_exist: bool = False,
+    must_be_in_parent=True,
+) -> Path:
+    """Parse input string as :py:class:`pathlib.Path` object.
+
+    Args:
+        input_path: Input string path that can be a relative or absolute path.
+        parent: Optional parent path of the input path
+        must_exist: Optional argument to check if the input path exists.
+        must_be_in_parent: Optional argument to check if the input path is
+            subpath of parent path
+
+    Returns:
+        The input path that is an absolute path and a :py:class:`pathlib.Path` object.
+    """
+    pathlike = Path(input_path)
+    if parent:
+        pathlike = parent.joinpath(pathlike)
+        if must_be_in_parent:
+            try:
+                pathlike.relative_to(parent)
+            except ValueError:
+                raise ValueError(
+                    f"Input path {input_path} is not a subpath of parent {parent}"
+                )
+
+    return pathlike.expanduser().resolve(strict=must_exist)
