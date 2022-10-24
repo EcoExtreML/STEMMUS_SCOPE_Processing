3aef498512457011142d003191cbd38749616c66
SarahAlidoost <f.alidoost@esciencecenter.nl>
2022-10-24 15:17:34 +0200

    fix pylint order of imports

--- PyStemmusScope/save.py
+++ PyStemmusScope/save.py
@@ -24,7 +24,7 @@ from typing import Union
 import numpy as np
 import pandas as pd
 import xarray as xr
-from PyStemmusScope import forcing_io, config_io
+from PyStemmusScope import config_io, forcing_io
 from . import variable_conversion as vc
 
 
