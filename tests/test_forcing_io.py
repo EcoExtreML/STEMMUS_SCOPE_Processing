b4fdd77d64fd6a3e3bad58af8097e3724181e1c7
Bart Schilperoort <b.schilperoort@gmail.com>
2023-02-21 17:01:46 +0100

    Fix test returning a value

--- tests/test_forcing_io.py
+++ tests/test_forcing_io.py
@@ -129,7 +129,7 @@ def test_dat_file_format(dat_files):
 
 def test_forcing_slice_NA():
     forcing_file = forcing_data_folder / "FI-Hyy_1996-2014_FLUXNET2015_Met.nc"
-    return forcing_io.read_forcing_data_plumber2(forcing_file, "NA", "NA")
+    forcing_io.read_forcing_data_plumber2(forcing_file, "NA", "NA")
 
 
 def test_full_routine(tmp_path, dat_files):
