e9596cebff2cd9eb6061ed7576d7354003d10d58
Mostafa Daoud <daoudmgm@linux668.itc.utwente.nl>
2025-02-24 16:27:30 +0100

    modify_variable_reference_names

--- PyStemmusScope/bmi/variable_reference.py
+++ PyStemmusScope/bmi/variable_reference.py
@@ -70,7 +70,7 @@ VARIABLES: tuple[BmiVariable, ...] = (
         output=True,
         units="cm s-1",
         grid=0,
-        keys=["ForcingData", "applied_infiltration"],
+        keys=["ForcingData", "infiltration"],
     ),
     BmiVariable(
         name="soil_evaporation",
